35e987471c2f0c4764e17afb71f19175e77189d2
Sandeep Pawar <sandeeppawar770@gmail.com>
2022-12-30 19:46:39 +0530

    Add files via upload

---
+++ Employee(Array Day 2)/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Employee_Array_Day_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Employee[] empArr= new Employee[3];
+            int empid = 0;
+            string? empname = "";
+            int salary = 0;
+            for (int i = 0; i < empArr.Length; i++)
+            {
+                Console.WriteLine("Enter the emp id");
+                empid  = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the emp name");
+                empname = Console.ReadLine();
+                Console.WriteLine("Enter the emp salary");
+                salary= Convert.ToInt32(Console.ReadLine());
+
+
+                empArr[i] = new Employee(empid, empname,salary);
+                
+               
+
+
+            }
+            Console.WriteLine(empArr[0].Name + " " + empArr[0].Id + " " + empArr[0].Salary);
+            Console.WriteLine(empArr[1].Name + " " + empArr[1].Id + " " + empArr[1].Salary);
+            Console.WriteLine(empArr[2].Name + " " + empArr[2].Id + " " + empArr[2].Salary);
+
+
+        }
+    }
+    public class Employee
+    {
+        public int id;
+        public string name;
+        public int salary;
+
+        public Employee(int id, string name, int salary)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Salary= salary;
+        }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Salary { get; set; }
+    }
+}
---
+++ Employee(Multiple Inheritance)/Program.cs
@@ -0,0 +1,182 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Employee_Multiple_Inheritance_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager objManage = new Manager("aman", 1, 300000.0m, "Manager");
+            Console.WriteLine("manager data");
+            Console.WriteLine(objManage.Name + " " + objManage.EmpNo + " " + objManage.DeptNo + " " + objManage.Basic + " " + objManage.CalcNetSalary() + " " + objManage.Designation);
+            GeneralManager objGeneral = new GeneralManager("swati", 2, 400000.0m,"general manager 1" ,"mercedes car");
+            Console.WriteLine("general manager");
+            Console.WriteLine(objGeneral.Name + " " + objGeneral.EmpNo + " " + objGeneral.DeptNo + " " + objGeneral.Basic + " " + objGeneral.CalcNetSalary() +" "+ objGeneral.Designation+" " + objGeneral.perks);
+            GeneralManager objGeneral1 = new GeneralManager("atharva", 3, 200000.0m,"general manager 2","bmw car");
+            Console.WriteLine("general manager");
+            Console.WriteLine(objGeneral1.Name + " " + objGeneral1.EmpNo + " " + objGeneral1.DeptNo + " " + objGeneral1.Basic + " " + objGeneral1.CalcNetSalary() +" "+ objGeneral.Designation + " "+ objGeneral1.perks);
+        }
+    }
+
+    public interface IDbFunctions{
+        public abstract decimal CalcNetSalary();
+    }
+
+
+    public abstract class Employee
+    {
+        string? name;
+        protected int empNo;
+        short deptNo;
+        protected decimal basic;
+        protected static int EMPNO = 1000;
+
+
+        public Employee(string Name, short DeptNo, decimal Basic)
+        {
+            this.Name = Name;
+            this.empNo = ++EMPNO;
+            this.DeptNo = DeptNo;
+            this.Basic = Basic;
+        }
+
+        public string? Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value?.Trim().Length > 0)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Name input is invalid");
+                }
+
+            }
+        }
+        public int EmpNo
+        {
+            get
+            { 
+                return empNo;
+            }
+        }
+        public short DeptNo
+        {
+            get 
+            { 
+                return deptNo;
+            }
+            set 
+            { 
+                if(value > 0)
+                {
+                    deptNo = value;
+                }
+                else
+                {
+                    Console.WriteLine("department input is invalid");
+                }
+               
+            }
+        }
+
+        public abstract decimal Basic { get; set; }
+
+        public abstract decimal CalcNetSalary();
+
+
+    }
+
+
+    public class Manager : Employee, IDbFunctions
+    {
+        string? designation;
+
+        public Manager(string Name, short DeptNo, decimal Basic, string? designation) : base(Name, DeptNo, Basic)
+        {
+           
+            this.Designation = designation;
+        }
+
+        public string? Designation
+        {
+            get
+            {
+                return designation;
+            }
+            set
+            {
+                if(value?.Trim().Length > 0)
+                {
+                    designation = value.Trim();
+                }
+                else
+                {
+                    Console.WriteLine("designation value is invalid");
+                }
+            }
+        }
+        public override decimal Basic { get; set; }
+
+        decimal sum = 0.0m;
+        public override decimal CalcNetSalary()
+        {
+            sum = Basic + 12000.0m;
+
+
+            return sum;
+        }
+    }
+
+    public class GeneralManager : Manager, IDbFunctions
+    {
+        public string? perks;
+
+        public GeneralManager(string Name, short DeptNo, decimal Basic, string? designation, string? perks) : base(Name, DeptNo, Basic, designation)
+        {
+            this.Perks = perks;
+        }
+
+        public string? Perks
+        {
+            get 
+            { 
+                return perks;
+            }
+            set
+            {
+                perks = value?.Trim();
+            }
+        }
+
+        public override decimal Basic { get ; set ; }
+
+        public override decimal CalcNetSalary()
+        {
+            return Basic + 24000.0m;
+        }
+    }
+
+    public class CEO : Employee, IDbFunctions
+    {
+        public CEO(string Name, short DeptNo, decimal Basic) : base(Name, DeptNo, Basic)
+        {
+
+        }
+
+        public override decimal Basic { get ; set ; }
+
+        public override sealed decimal CalcNetSalary()
+        {
+           return Basic + 50000.0m;
+        }
+    }
+
+
+}
